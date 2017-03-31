abbccb6fce51a531fd4399532c87b653c6dfcab9
dvdmrn <dgm.marino@gmail.com>
2017-03-30 20:52:37 -0700

    extended webscraper to parse arbitrary subreddits using CL input

---
+++ webscraper/redditscraper.py
@@ -0,0 +1,60 @@
+"""
+Scrapes data from an arbitrary subreddit.
+
+Use a CL argument to specify the subreddit
+    EG: `python redditscraper.py canada`
+        `python redditscraper.py ubc`
+TODO: get textual data from comment threads
+TODO: get an API key
+"""
+
+from lxml import html
+import requests
+import time
+import sys
+import csv
+# We must specify a non-public user-agent to acquire data from reddit's servers.
+# It will probably be good practice to get an API key for the future but this
+# currently works fine.
+
+filepath = "data/"
+
+def zeropad(n): 
+    if(n<10):
+        return "0"+str(n)
+    else:
+        return str(n)
+
+def printTitles(subreddit):
+    """
+    prints the titles from the frontpage of r/canada
+    """
+    page = requests.get('http://reddit.com/r/'+subreddit,headers = {'user-agent': 'SOROS'})
+    tree = html.fromstring(page.content)
+
+    # a list of titles constructed from the title element's xpath
+    titles = tree.xpath('//p[@class="title"]/a/text()')
+
+    return titles
+
+def makeFileName(subreddit,date):
+    return "r_  "+subreddit+"_"+date
+
+def main():
+    currentdatetime = str(time.localtime()[1])+"-"+str(time.localtime()[2])+"-"+str(time.localtime()[0])+"_"+str(time.localtime()[3])+":"+str(time.localtime()[4])+":"+zeropad(time.localtime()[5])
+    subreddit = sys.argv[1]    
+    print "scraping r/"+subreddit+" at: "+str(currentdatetime)
+    
+    titles = printTitles(subreddit)
+    encodedTitles = []
+    for t in titles:
+        encodedTitles.append(t.encode('utf-8'))
+    fileName = makeFileName(subreddit,currentdatetime)+".csv"
+       
+    with open(filepath+fileName, 'wb') as f:
+        writer = csv.writer(f)
+        for t in encodedTitles:
+            writer.writerow([t])
+
+
+main()
